fbb3278b1fe91e858f39f4c1bb11fde48d01cb0a
PratikT112 <pratiktelang112@gmail.com>
2025-08-15 19:19:55 +0530

    Jackson Deserializer Init

---
+++ src/main/java/com/pratikt112/correbankingsystembe/config/JacksonConfig.java
@@ -0,0 +1,34 @@
+package com.pratikt112.correbankingsystembe.config;
+
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class JacksonConfig {
+    public static class EmptyStringtoNullDeserializer extends JsonDeserializer<String>{
+
+        @Override
+        public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+            String value = jsonParser.getValueAsString();
+            return (value != null && value.isEmpty()) ? null : value;
+        }
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(String.class, new EmptyStringtoNullDeserializer());
+        mapper.registerModule(module);
+        return mapper;
+    }
+}
