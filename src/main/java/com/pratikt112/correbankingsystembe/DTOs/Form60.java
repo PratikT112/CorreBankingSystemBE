1b68558128062c9672b3b3a66bad947694d93243
PratikT112 <pratiktelang112@gmail.com>
2025-10-04 23:58:26 +0530

    small commit for F60 applicable

--- src/main/java/com/pratikt112/correbankingsystembe/DTOs/Form60.java
+++ src/main/java/com/pratikt112/correbankingsystembe/DTOs/Form60.java
@@ -3,6 +3,7 @@ package com.pratikt112.correbankingsystembe.DTOs;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -18,4 +19,14 @@ public class Form60 {
     private String panAppliedFlag; // "pan-appl-yes" | "pan-appl-no"
     private LocalDate panAppliedDate;
     private String panAckNo;
+
+    public boolean isApplicable() {
+        return subDate != null
+                || tranDate != null
+                || agriIncome != null
+                || otherIncome != null
+                || panAppliedFlag != null
+                || panAppliedDate != null
+                || panAckNo != null;
+    }
 }
