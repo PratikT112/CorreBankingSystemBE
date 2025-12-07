02fbbf87d807e1591fe291e9d7a961589149dd70
PratikT112 <pratiktelang112@gmail.com>
2025-12-08 00:42:02 +0530

    Cust-Amendment-Fetch-Aggregator

--- src/main/java/com/pratikt112/correbankingsystembe/service/CustomerFetchAggregator.java
+++ src/main/java/com/pratikt112/correbankingsystembe/service/CustomerFetchAggregator.java
@@ -3,6 +3,7 @@ package com.pratikt112.correbankingsystembe.service;
 import com.pratikt112.correbankingsystembe.DTOs.CobData;
 import com.pratikt112.correbankingsystembe.DTOs.OvdDetails;
 import com.pratikt112.correbankingsystembe.DTOs.PersonName;
+import com.pratikt112.correbankingsystembe.exception.RecordNotFoundException;
 import com.pratikt112.correbankingsystembe.model.cmob.Cmob;
 import com.pratikt112.correbankingsystembe.model.cmob.CmobId;
 import com.pratikt112.correbankingsystembe.model.cr60.Cr60;
@@ -35,17 +36,37 @@ public class CustomerFetchAggregator {
 
     public CobData fetchCobDB(String cifNo, CobData newCobData){
 
-        try {
-            Optional<Cusm> fetchedCusm = cusmRepo.findById(new CusmId("003", cifNo));
-            Optional<Cr60> fetchedCr60 = cr60Repo.findById("003" + cifNo);
-            Optional<Cmob> fetchedCmob = cmobRepo.findById(new CmobId("003", cifNo, "P"));
-            Optional<Cuid> fetchedCuid = cuidRepo.findById(new CuidId("003", cifNo, newCobData.getCustOvdDetails().stream().filter(x->"Y".equals(x.getOvdDocType())).findFirst().map(OvdDetails::getOvdDocType).orElse("")));
-            Optional<Cusvaa> fetchedCusvaa = cusvaaRepo.findById(new CusvaaId("003", cifNo, "0001"));
-            Optional<Cusvdd> fetchedCusvdd = cusvddRepo.findById("003"+cifNo);
-        } catch (Exception e) {
-            log.error("Fetch failed for some table");
-        }
-        return null;
+        Cusm fetchedCusm = cusmRepo.findById(new CusmId("003", cifNo))
+                .orElseThrow(()->{
+                    log.error("CUSM not found for CIF: {}", cifNo);
+                    return new RecordNotFoundException("CUSM", cifNo);
+                });
+        Cr60 fetchedCr60 = cr60Repo.findById("003" + cifNo)
+                .orElseThrow(()->{
+                    log.error("CR60 not found for CIF: {}", cifNo);
+                    return new RecordNotFoundException("CR60", cifNo);
+                });
+        Cmob fetchedCmob = cmobRepo.findById(new CmobId("003", cifNo, "P"))
+                .orElseThrow(()->{
+                    log.error("CMOB not found for CIF: {}", cifNo);
+                    return new RecordNotFoundException("CMOB", cifNo);
+                });
+        Cuid fetchedCuid = cuidRepo.findById(new CuidId("003", cifNo, newCobData.getCustOvdDetails().stream().filter(x->"Y".equals(x.getOvdDocType())).findFirst().map(OvdDetails::getOvdDocType).orElse("")))
+                .orElseThrow(()->{
+                    log.error("CUID not found for CIF: {}", cifNo);
+                    return new RecordNotFoundException("CUID", cifNo);
+                });
+        Cusvaa fetchedCusvaa = cusvaaRepo.findById(new CusvaaId("003", cifNo, "0001"))
+                .orElseThrow(()->{
+                    log.error("CUSVAA not found for CIF: {}", cifNo);
+                    return new RecordNotFoundException("CUSVAA", cifNo);
+                });
+        Cusvdd fetchedCusvdd = cusvddRepo.findById("003"+cifNo)
+                .orElseThrow(()->{
+                    log.error("CUSVDD not found for CIF: {}", cifNo);
+                    return new RecordNotFoundException("CUSVDD", cifNo);
+                });
+
     }
 
     private CobData constructCobToCompare(Cusm fetchedCusm, Cr60 fetchedCr60, Cmob fetchedCmob, Cuid fetchedCuid, Cusvaa fetchedCusvaa, Cusvdd fetchedCusvdd){
@@ -63,11 +84,35 @@ public class CustomerFetchAggregator {
                 fetchedCusvaa.getMidName(),
                 fetchedCusvaa.getName2()));
         constructed.setCustDob(fetchedCusvdd.getBirthDate1());
-
-
-
+        constructed.setCustGender(fetchedCusvdd.getSexCode());
+        constructed.setCustMaritalStatus(fetchedCusvdd.getMaritalStatus());
+        constructed.setCustFatherName(getPersonNameFromCombinedName(fetchedCusvaa.getFatherTitle(), fetchedCusvaa.getFatherName()));
+        constructed.setCustMotherName(getPersonNameFromCombinedName(fetchedCusvaa.getMotherTitle(), fetchedCusvaa.getMotherName()));
+        constructed.setCustSpouseName(getPersonNameFromCombinedName(fetchedCusvaa.getSpouseTitle(), fetchedCusvaa.getSpouseName()));
+        constructed.setCustNationality(fetchedCusvaa.getCountryCode());
+        constructed.setCustOccType(fetchedCusvdd.getOccupationCode());
+        constructed.setCustOccSubType(fetchedCusvdd.getOccupDesc());      //To be checked
+//        constructed.setCustAnnIncome(fetchedCusva);
 
         return constructed;
+    }
+
+    private PersonName getPersonNameFromCombinedName(String titleCode, String combinedName){
+        PersonName name = new PersonName(titleCode, null, null,null);
+        String[] s = combinedName.split(" ");
+        long count = combinedName.chars().filter(x -> x == ' ')
+                .count();
+        if(count == 2){
+            name.setFirstName(s[0]);
+            name.setMiddleName(s[1]);
+            name.setLastName(s[2]);
+        } else if(count == 1){
+            name.setFirstName(s[0]);
+            name.setLastName(s[1]);
+        } else {
+            name.setFirstName(combinedName);
+        }
+        return name;
     }
 
 }
