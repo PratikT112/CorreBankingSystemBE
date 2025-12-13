9851b30560962235caa434ff9b2f8fdf74f95173
PratikT112 <pratiktelang112@gmail.com>
2025-12-13 18:25:51 +0530

    turn-db-init-2

--- src/main/java/com/pratikt112/correbankingsystembe/model/turn/Turn.java
+++ src/main/java/com/pratikt112/correbankingsystembe/model/turn/Turn.java
@@ -1,4 +1,40 @@
 package com.pratikt112.correbankingsystembe.model.turn;
 
+
+import com.pratikt112.correbankingsystembe.utility.DdMmYyyyStringToLocalDateConverter;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TURN")
 public class Turn {
+
+    @EmbeddedId
+    private TurnId id;
+
+    @NotNull
+    @Digits(integer = 17, fraction = 3, message = "Annual Income / TurnOver must be of maximum 17 digits")
+    @Column(name = "TURNOVER", nullable = false, precision = 17, scale = 3)
+    private BigDecimal turnOver;
+
+    @NotNull
+    @Pattern(regexp = "[YN]")
+    @Column(name = "CONSENT_DATA", nullable = false, length = 1)
+    private String consentData = "N";
+
+    @NotBlank(message = "CONSENT_DATA_DT must not be blank")
+    @Column(name = "CONSENT_DATA_DT", nullable = false, length = 8)
+    @Convert(converter = DdMmYyyyStringToLocalDateConverter.class)
+    private LocalDate consentDataDt;
 }
--- src/main/java/com/pratikt112/correbankingsystembe/model/turn/TurnId.java
+++ src/main/java/com/pratikt112/correbankingsystembe/model/turn/TurnId.java
@@ -1,4 +1,28 @@
 package com.pratikt112.correbankingsystembe.model.turn;
 
-public class TurnId {
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TurnId implements Serializable {
+
+    @NotBlank
+    @Column(name = "SOC_NO", length = 3)
+    private String socNo;
+
+    @NotBlank
+    @Size(max = 16)
+    @Column(name = "CUST_NO", length = 16)
+    private String custNo;
 }
